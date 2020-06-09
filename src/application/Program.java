7475fca69c1f2969699e1a86baff9b949d3e2bfc
Eric Demate <eric.demate@gmail.com>
2020-06-09 20:48:51 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		//Primeiro programa no GitHub
+		
+		System.out.println("\\\\\\\\\\Primeiro pprograma no Github//////////");
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite seu nome: ");
+		String name = sc.next();
+		System.out.println();
+		System.out.println("Seu nome é "+ name +" ???");
+		System.out.println();
+		System.out.println("FIM");
+		sc.close();
+	}
+
+}
